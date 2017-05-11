3d6137321712d7d48020db07ea1562b3c18db489
Kaitaku Takeda <katakeda@ucsc.edu>
2017-05-10 19:18:01 -0700

    Merged branch dev_andy

--- nodejs/index.js
+++ nodejs/index.js
@@ -4,5 +4,5 @@ const priceFinder = new PriceFinder();
 // Atoms for Peace : Amok  (from Amazon)
 const uri = 'http://www.amazon.com/Albanese-Candy-Sugar-Assorted-5-pound/dp/B00DE4GWWY?';
 priceFinder.findItemPrice(uri, function(err, price) {
-    console.log(price); // 8.91
+    console.log(price); // 18.91
 });
