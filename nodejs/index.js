37fcc9d6e42333f314a6893ae6dbeb84c24d5fd0
DioFlare Studios <DioFlare Studios>
2017-05-12 18:14:53 -0700

    tidying up

--- nodejs/index.js
+++
@@ -1,8 +0,0 @@
-const PriceFinder = require('price-finder');
-const priceFinder = new PriceFinder();
-
-// Atoms for Peace : Amok  (from Amazon)
-const uri = 'http://www.amazon.com/Albanese-Candy-Sugar-Assorted-5-pound/dp/B00DE4GWWY?';
-priceFinder.findItemPrice(uri, function(err, price) {
-    console.log(price); // 8.91
-});
