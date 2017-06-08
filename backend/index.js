221c365d32ed6f7a141dc92c394d30a8c76500b2
Rysm <andyliwang95@gmail.com>
2017-06-07 21:29:58 -0700

    orking nodejs server with CRON scheduling.

--- backend/index.js
+++ backend/index.js
@@ -1,20 +1,38 @@
+//Explicitly bring in packages needed
 const express = require('express');
-
 const app = express();
 
+//Allows for timed jobs by schedule - even date/time
+var CronJob = require('cron').CronJob;
+
+//Initial expressJS page grab by client request
 app.get('/', (req, res) => {
 
+        const PriceFinder = require('price-finder');
+        const priceFinder = new PriceFinder();
+        const uri = 'http://www.amazon.com/Albanese-Candy-Sugar-Assorted-5-pound/dp/B00DE4GWWY?';
 
-  const PriceFinder = require('price-finder');
-  const priceFinder = new PriceFinder();
+        priceFinder.findItemPrice(uri, function(err, price) {
+            console.log("Current price " +   price);
+            res.status(200).send("Price Of Gummies: " + price)//.end();
+        });
 
-  // Atoms for Peace : Amok  (from Amazon)
-  const uri = 'http://www.amazon.com/Albanese-Candy-Sugar-Assorted-5-pound/dp/B00DE4GWWY?';
-  priceFinder.findItemPrice(uri, function(err, price) {
-      console.log(price);
-      res.status(200).send("Price Of Gummies: " + price).end();
-  });
+        amazonJob.start();
+});
 
+
+//Schedule an amazon job.
+var amazonJob = new CronJob('* 1 * * * * *', function(req, res){
+
+    const PriceFinder = require('price-finder');
+    const priceFinder = new PriceFinder();
+    const uri = 'http://www.amazon.com/Albanese-Candy-Sugar-Assorted-5-pound/dp/B00DE4GWWY?';
+
+    priceFinder.findItemPrice(uri, function(err, price) {
+
+        console.log("updated every minute: " + price);
+
+    });
 });
 
 // Start the server
