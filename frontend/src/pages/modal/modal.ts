815503d54f95d1c718792dc033b3b5c1a2a41179
Kaitaku Takeda <katakeda@ucsc.edu>
2017-06-11 22:08:51 -0700

    Minor change

--- frontend/src/pages/modal/modal.ts
+++ frontend/src/pages/modal/modal.ts
@@ -16,6 +16,8 @@ export class Modal {
   serverAPI: any = 'http://lithe-climber-167308.appspot.com/';
   // Local server for development
   localAPI: any = 'http://localhost:8080/';
+  // Url to be searched
+  searchUrl: any = '';
   // Keyword to be searched
   searchKey: any = '';
   // List returned in response of item search
