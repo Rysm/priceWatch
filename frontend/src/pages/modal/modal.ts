dee71181d00006b7ac46f1f8c62ac063de08d5e4
Rysm <andyliwang95@gmail.com>
2017-06-13 20:47:38 -0700

    index update

--- frontend/src/pages/modal/modal.ts
+++ frontend/src/pages/modal/modal.ts
@@ -14,6 +14,7 @@ export class Modal {
 
   // Our server hosted on Google
   serverAPI: any = 'http://lithe-climber-167308.appspot.com/';
+  
   // Local server for development
   localAPI: any = 'http://localhost:8080/';
   // Url to be searched
